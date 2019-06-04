76294ed39bb7a4e928ff9ac7f7e5bc70c95fb441
Tahir Ali <tahirali.sds@nxvt.com>
2019-06-04 15:21:27 +0500

    components and databinding and directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('My Recipe', 'Test Description', 'https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG'),
     new Recipe('My Recipe 2', 'Test Description 2', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg')
@@ -14,6 +15,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelect(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
