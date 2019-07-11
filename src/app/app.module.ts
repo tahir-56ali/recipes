8e1e24a099d2620c63f12cae91d097ca1d0dcd28
Tahir Ali <tahirali.sds@nxvt.com>
2019-07-11 22:38:17 +0500

    edit recipe form using reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -14,7 +15,6 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,6 +33,7 @@ import {FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Params} from '@angular/router';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,7 +11,8 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) { }
+  recipeForm = FormGroup;
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params
@@ -17,8 +20,54 @@ export class RecipeEditComponent implements OnInit {
             (params: Params) => {
               this.id = +params['id'];
               this.editMode = params['id'] != null;
+              this.initForm();
             }
         );
+  }
+    onSubmit() {
+      console.log(this.recipeForm);
+    }
+    addIngredient() {
+        (<FormArray>this.recipeForm.get('ingredients')).push(
+            new FormGroup({
+                'name': new FormControl(null, Validators.required),
+                'amount': new FormControl(null, [
+                    Validators.required,
+                    Validators.pattern(/^[1-9]+[0-9]*$/)
+                ])
+            })
+        );
+    }
+  initForm() {
+      let recipeName = '';
+      let recipeImagePath = '';
+      let recipeDescription = '';
+      let recipeIngredients = new FormArray([]);
+      const recipe = this.recipeService.getRecipe(this.id);
+      if (this.editMode) {
+          recipeName = recipe.name;
+          recipeImagePath = recipe.imagePath;
+          recipeDescription = recipe.description;
+          if (recipe['ingredients']) {
+              for (let ingredient of recipe.ingredients) {
+                  recipeIngredients.push(
+                      new FormGroup({
+                      'name': new FormControl(ingredient.name, Validators.required),
+                      'amount': new FormControl(ingredient.amount, [
+                          Validators.required,
+                          Validators.pattern(/^[1-9]+[0-9]*$/)
+                      ])
+                  })
+                );
+              }
+          }
+      }
+      this.recipeForm = new FormGroup({
+          'name': new FormControl(recipeName, Validators.required),
+          'imagePath': new FormControl(recipeImagePath, Validators.required),
+          'description': new FormControl(recipeDescription, Validators.required),
+          'ingredients': recipeIngredients
+      });
   }
 
 }
