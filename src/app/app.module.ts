591d8cce30c0417a3a7737405f19f40e1bfa0b5e
tahir-56ali <tahir.56ali@gmail.com>
2019-08-04 00:45:11 +0500

    recipes module lazy loading and core module for header and home compnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,27 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './header/header.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
 import {DataStorageService} from './shared/data-storage.service';
 import {AuthService} from './auth/auth.service';
 import {AuthGuard} from './auth/auth-guard.service';
-import {RecipesModule} from './recipes/recipes.module';
-import {SharedModule} from './shared/shared.module';
+import {AuthModule} from "./auth/auth.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
-import {AuthModule} from './auth/auth.module';
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
-import {DataStorageService} from '../shared/data-storage.service';
-import {Recipe} from "../recipes/recipe.model";
-import {AuthService} from '../auth/auth.service';
+import {DataStorageService} from '../../shared/data-storage.service';
+import {Recipe} from "../../recipes/recipe.model";
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
