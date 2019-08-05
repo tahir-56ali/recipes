b90388eb39f6d9128a15b0e38e334dea9752d0ef
Tahir Ali <tahirali.sds@nxvt.com>
2019-08-05 23:23:32 +0500

    moving services from app module to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,10 @@ import {HomeComponent} from "./home/home.component";
 import {SharedModule} from "../shared/shared.module";
 import {AppRoutingModule} from "../app-routing.module";
 import {CommonModule} from "@angular/common";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {RecipeService} from "../recipes/recipe.service";
+import {DataStorageService} from "../shared/data-storage.service";
+import {AuthService} from "../auth/auth.service";
 
 @NgModule({
     declarations: [
@@ -19,6 +23,12 @@ import {CommonModule} from "@angular/common";
     exports: [
         HeaderComponent,
         AppRoutingModule
+    ],
+    providers: [
+      ShoppingListService,
+      RecipeService,
+      DataStorageService,
+      AuthService
     ]
 })
 export class CoreModule { }
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -22,6 +22,7 @@ const recipesRoutes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(recipesRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class RecipesRoutingModule { }
