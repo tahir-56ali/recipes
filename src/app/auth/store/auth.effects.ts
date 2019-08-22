a23b4d7f5642781a8e85bc2f6a7fe4a934e6088c
tahir-56ali <tahir.56ali@gmail.com>
2019-08-23 02:01:51 +0500

    TRY_SIGNUP auth action

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Actions, Effect} from '@ngrx/effects';
 
+import * as AuthActions from './auth.actions';
+
 @Injectable()
 export class AuthEffects {
   @Effect()
-  authSignup;
+  authSignup = this.actions$.ofType(AuthActions.TRY_SIGNUP);
 
   constructor(private actions$: Actions) {
   }
