2ac7ae749da09e865c3efc49207d8a4673ca758e
tahir-56ali <tahir.56ali@gmail.com>
2019-08-26 01:39:03 +0500

    added routerstore package and storedevtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {EffectsModule} from "@ngrx/effects";
 import {StoreModule} from "@ngrx/store";
+import {StoreRouterConnectingModule} from "@ngrx/router-store";
+import {AuthEffects} from "./auth/store/auth.effects";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AuthModule} from "./auth/auth.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {CoreModule} from "./core/core.module";
 import { reducers } from "./store/app.reducers";
-import {AuthEffects} from "./auth/store/auth.effects";
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import {AuthEffects} from "./auth/store/auth.effects";
     AuthModule,
     CoreModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule.forRoot(),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   bootstrap: [AppComponent]
 })
